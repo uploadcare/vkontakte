8781e113bcde0a436818fb14295d2aa675edc4c9
Andrey Baryshnikov <andrewshkovskii@gmail.com>
2019-03-01 16:14:36 +0800

    Fix httplib import

--- vkontakte/http.py
+++ vkontakte/http.py
@@ -1,7 +1,8 @@
 #coding: utf-8
+
 from __future__ import with_statement
 from contextlib import closing
-import httplib
+from six.moves import http_client
 
 # urllib2 doesn't support timeouts for python 2.5 so
 # custom function is used for making http requests
@@ -9,9 +10,9 @@ import httplib
 def post(url, data, headers, timeout, secure=False):
     host_port = url.split('/')[2]
     timeout_set = False
-    connection = httplib.HTTPSConnection if secure else httplib.HTTPConnection
+    connection = http_client.HTTPSConnection if secure else http_client.HTTPConnection
     try:
-        connection = connection(host_port, timeout = timeout)
+        connection = connection(host_port, timeout=timeout)
         timeout_set = True
     except TypeError:
         connection = connection(host_port)
@@ -24,4 +25,4 @@ def post(url, data, headers, timeout, secure=False):
 
         connection.request("POST", url, data, headers)
         response = connection.getresponse()
-        return (response.status, response.read())
+        return response.status, response.read()
